1ec193d39487e26603e6c570316ecf5e3b6baa72
tom burns <tomburns.gj@gmail.com>
2023-03-12 15:19:09 +0100

    home and about pages set up and linked - navbar stimulus added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#show", page: "home", as: :home
 
+  get "/pages/:page", to: "pages#show", as: :page
   # Defines the root path route ("/")
   # root "articles#index"
 end
